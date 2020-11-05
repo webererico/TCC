540a7a2841664b356580c76fb09c1e8876b8cefa
Erico Weber <ericoweber@hotmail.com>
2020-11-04 22:15:35 -0200

    menu alteracao variaveis IHC

---
+++ codigosTestes/testeEncoder.py
@@ -0,0 +1,30 @@
+# CODIGO DE TESTE DO ENCODER GIRATORIO
+
+from RPi import GPIO
+from time import sleep
+
+clk = 21
+dt = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+counter = 0
+clkLastState = GPIO.input(clk)
+
+try:
+
+        while True:
+                clkState = GPIO.input(clk)
+                dtState = GPIO.input(dt)
+                if clkState != clkLastState:
+                        if dtState != clkState:
+                                counter += 1
+                        else:
+                                counter -= 1
+                        print (counter)
+                clkLastState = clkState
+                sleep(0.01)
+finally:
+        GPIO.cleanup()
